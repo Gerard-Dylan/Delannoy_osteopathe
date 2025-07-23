929ab108114521374d52c4e286452683cc03436c
Dylan-Gerard <d.marcgerard@gmail.com>
2025-07-23 15:35:23 +0200

    feat: vérification de session

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,6 +3,14 @@ import argon2 from "argon2";
 import { getUserByEmail } from "../models/user.model";
 import { generateToken } from "../services/jwt.service";
 
+// Type personnalisé pour req.auth injecté par le middlewares
+interface RequestWithAuth extends Request {
+    auth?: {
+        id_user: number;
+    };
+}
+
+// Connexion de l’admin
 export async function loginAdmin(req: Request, res: Response) {
     const { email, password } = req.body;
 
@@ -14,13 +22,13 @@ export async function loginAdmin(req: Request, res: Response) {
         const user = await getUserByEmail(email);
 
         if (!user) {
-            return res.status(401).json({ error: "Identifiant invalide" });
+            return res.status(401).json({ error: "Identifiants invalides" });
         }
 
         const passwordValid = await argon2.verify(user.password_hash, password);
 
         if (!passwordValid) {
-            return res.status(401).json({ error: "Mot de passe invalide" });
+            return res.status(401).json({ error: "Identifiants invalides" });
         }
 
         const token = generateToken({ id_user: user.id_user });
@@ -32,9 +40,21 @@ export async function loginAdmin(req: Request, res: Response) {
             maxAge: 1000 * 60 * 60 * 2, // 2h
         });
 
-        res.status(200).json({ message: "Connexion réussie" });
+        return res.status(200).json({ message: "Connexion réussie" });
     } catch (err) {
         console.error("Erreur login:", err);
-        res.status(500).json({ error: "Erreur serveur" });
+        return res.status(500).json({ error: "Erreur serveur" });
     }
+}
+
+//  Récupération du profil
+export function getProfile(req: RequestWithAuth, res: Response) {
+    if (!req.auth?.id_user) {
+        return res.status(401).json({ error: "Non authentifié" });
+    }
+
+    return res.status(200).json({
+        id_user: req.auth.id_user,
+        isAdmin: true,
+    });
 }
